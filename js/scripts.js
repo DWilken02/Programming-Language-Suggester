ccfe083143fb9aceb2b614624c6caadec2b1d166
DWilken02 <Dylan.Wilken06@gmail.com>
2023-10-13 15:57:01 -0700

    WIP add JavaScript to Project

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//UI Logic
+window.addEventListener("load" function() {
+  document.getElementById("question-form").addEventListener("submit", handleFormSubmission);
+})
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+}
