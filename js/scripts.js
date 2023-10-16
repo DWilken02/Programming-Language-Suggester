e1756441ad226d68e60cddc1ee4bd63e7b4bde6c
DWilken02 <Dylan.Wilken06@gmail.com>
2023-10-16 11:36:48 -0700

    WIP add question values to JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 //UI Logic
-window.addEventListener("load" function() {
+window.addEventListener("load", function() {
   document.getElementById("question-form").addEventListener("submit", handleFormSubmission);
-})
+});
 
 function handleFormSubmission(event) {
   event.preventDefault();
+  const question1Input = document.getElementById("question1").value;
+  const question2Input = document.getElementById("question2").value;
+  const question3Input = document.getElementById("question3").value;
+  const question4Input = document.getElementById("question4").value;
+  const question5Input = document.getElementById("question5").value;
 }
